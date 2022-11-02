7b9f67bd008c0a4fe096afd1ccbc59995bc9ebd0
Enos Gabriel <enosgb55@gmail.com>
2022-11-02 13:46:12 -0300

    allow cors

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const app = require("./src/app.js");
 const swaggerUI = require("swagger-ui-express");
 const swaggerFile = require("./swagger_output.json");
+const cors = require("cors");
 
 const port = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
